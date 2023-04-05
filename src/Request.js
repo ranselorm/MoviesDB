c1e2f18183211fdd18811b7e6abf88bab40a53f5
ranselorm <ranselorm@outlook.com>
2023-04-05 05:07:33 +0000

    finish Main component

---
+++ src/Request.js
@@ -0,0 +1,11 @@
+const key = "9d1ab418a180e3cfc24e6df03235c97c";
+
+const requests = {
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/tp_rated?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestHorror: `https://api.themoviedb.org/3/movie/horror?api_key=${key}&language=en-US&page=1`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests;
---
+++ src/components/Main.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import requests from "../Request";
+
+const Main = () => {
+  const [movies, setMovies] = useState([]);
+  const movie = movies[Math.floor(Math.random() * movies.length)];
+  useEffect(() => {
+    axios.get(requests.requestPopular).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, []);
+
+  //   console.log(movie);
+  const truncatedString = (string, number = 200) => {
+    if (string?.length > number) {
+      return string.slice(0, number) + "...";
+    } else {
+      return string;
+    }
+  };
+
+  return (
+    <>
+      <div className="w-full h-[550px]">
+        <div className="w-full h-full">
+          <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
+          <img
+            className="w-full h-full object-cover"
+            src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+            alt={movie?.title}
+          />
+
+          <div className="absolute w-full top-[20%] p-4 md:p-8">
+            <h1 className="text-3xl md:text-5xl font-bold">{movie?.title}</h1>
+            <div className="my-8">
+              <button className="border bg-gray-400 text-black border-gray-300 py-2 px-5">
+                Play
+              </button>
+              <button className="border  text-white border-gray-400 py-2 px-5 ml-4">
+                Watch Later
+              </button>
+            </div>
+            <p className="text-sm text-gray-300">
+              Released: {movie?.release_date}
+            </p>
+            <p className="w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200">
+              {truncatedString(movie?.overview)}
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Main;
